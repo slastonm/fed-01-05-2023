5a01ddd2aada78bfde9929d542414f5248b298ff
Max S <slastonm@gmail.com>
2023-05-01 22:13:58 +0300

    cw 01

---
+++ 01/math.js
@@ -0,0 +1,28 @@
+let firstValue = prompt('First value'); // string
+let secondValue = prompt('Second value'); // string
+// let summValue = firstValue + secondValue;
+// let summValue = +firstValue + +secondValue;
+//let summValue =parseInt(firstValue) + parseInt(secondValue); // parseInt конвертує до цілого числа
+// let summValue =parseFloat(firstValue) + parseFloat(secondValue);
+let summValue =Number(firstValue) + Number(secondValue);
+
+let numberFirstValue = Number(firstValue);
+let numberSecondValue = Number(secondValue);
+
+console.log(numberFirstValue + numberSecondValue);
+console.log(numberFirstValue - numberSecondValue);
+console.log(numberFirstValue / numberSecondValue);
+console.log(numberFirstValue * numberSecondValue);
+
+let productPrice = 100;
+let productBprice = 120;
+let productCprice = 180;
+let totlPriceFiveProduct = 5*productPrice; // 500
+console.log(totlPriceFiveProduct);
+
+let totalPrice = productPrice + productBprice + productCprice;
+let middlePrice = (productPrice + productBprice + productCprice)/3;
+console.log(middlePrice);
+
+
+
---
+++ 01/my-script.js
@@ -0,0 +1,78 @@
+// alert('Hello from js file');
+
+// variable
+var oldValue = '#333';
+let newVariableName = 'Hello world';
+const srtrictVar = 3;
+
+let NewVaribableName = 'Hello world 2222!';
+
+// console.log(newVariableName);
+// console.log(NewVaribableName);
+
+let nullValue = 1111;
+let ueserName = 'Test';
+let UserAge = 100;
+const KEYVALUE = '1234567890abcdef';
+let user_name = 'Test';
+let VarValue = 123;
+
+let firstNumber = 1;
+let secondNumber = 2;
+// console.log(firstNumber-secondNumber);
+
+let a = 2;
+let b = 3;
+let c = a+b;
+
+// console.log(c);
+
+// Number
+let numberValue = 123;
+let numberFloat = 0.007;
+let infinityValue = Infinity; // Infinity
+let notAnumberValue = NaN;
+let maxValue = Number.MAX_VALUE;
+let minValue = Number.MIN_VALUE;
+
+console.log("Max value: " + maxValue);
+console.log(" min value: " + minValue);
+
+//String
+
+let firstName ='John';
+let secondName ="Smith";
+console.log(firstName);
+console.log(secondName);
+// let stringNumber = '10';
+numberValue = '10';
+
+// 1>=2 = false
+// Boolean
+let booleanValue = true;
+let falseVale = false;
+
+let isAdmin = false;
+let isLoginUser = true;
+
+
+let ageValue = 18;
+let stringAgeValue = '20';
+let summTotalAge = ageValue + stringAgeValue;
+console.log(summTotalAge); //string + number = string
+console.log(ageValue-stringAgeValue);
+console.log(ageValue*stringAgeValue);
+console.log(stringAgeValue/ageValue);
+
+let noValue = null;
+let notFindValue = undefined;
+let listElementsAll = document.querySelector('li');
+console.log(listElementsAll);
+// console.log(courseName);
+let dayInfo = prompt('Як ваш день?'); // "" ''
+// console.log(dayInfo);
+let userDayInfo = dayInfo;
+console.log('Дані з userDayInfo',userDayInfo);
+
+
+
